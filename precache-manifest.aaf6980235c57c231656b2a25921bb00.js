d2d5f10a8e6d670f1ea9130da669d3150ba5a897
Nyar9646 <wmgmgmgw@gmail.com>
2021-12-02 20:00:32 +0900

    Updates

---
+++ precache-manifest.aaf6980235c57c231656b2a25921bb00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc58b014fc06b8b35aab85857c09894d",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "ee41c9604c4dcc393a33",
+    "url": "/todoapp/static/js/2.a5e6a60e.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/todoapp/static/js/2.a5e6a60e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1949aad3a68fc7e1beb",
+    "url": "/todoapp/static/js/main.e01a39a3.chunk.js"
+  },
+  {
+    "revision": "80b05f8f46ecf0bf2c2b",
+    "url": "/todoapp/static/js/runtime-main.5545d9fc.js"
+  }
+]);
